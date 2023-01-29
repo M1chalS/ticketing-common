1d09fbc25f23e606ba442d80fbf21b88d63aaf28
M1chalS <michalszajner@wp.pl>
2023-01-29 15:53:26 +0100

    fixed error with build

--- build/index.js
+++ build/index.js
@@ -1,7 +1,9 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const color = {
     red: 44,
     blue: 30,
     green: 120
 };
 console.log(color);
+exports.default = color;
